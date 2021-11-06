3716f9224bd48b1ce25b79b6d8ffbb769f1503e5
CSDUMMI <31551856+CSDUMMI@users.noreply.github.com>
2021-11-06 09:17:22 +0000

    User and Experience table/model added

--- src/database.py
+++ src/database.py
@@ -10,4 +10,15 @@ class BaseModel(pw.Model):
         database = database
 
 class User(BaseModel):
+    id = pw.UUIDField(primary_key = True)
+    name = pw.TextField(null = True)
+    languages = pw.ArrayField(CharField, default = ["eng","deu"])
     
+    password_hash = pw.CharField(max_length=64)
+    password_salt = pw.CharField(max_length=64)
+    
+class Experience(BaseModel):
+    topic = pw.CharField()
+    user = pw.ForeignKeyField(User, backref = "experience")
+    exp = pw.IntegerField()
+
